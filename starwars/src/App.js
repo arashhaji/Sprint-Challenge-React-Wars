c084b822406c1aabc18ea94a6a2799eef78d8b96
arashhaji <56742302+arashhaji@users.noreply.github.com>
2019-12-13 12:07:05 -0500

    StarWars Character
    Import useState and useEffect
    added hook
    created component
    map over characters

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+// import React from 'react';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import StarWarsChar from "./components/StarWarsChar"
 
 const App = () => {
+  const [starWarschar, setStarWarsChar] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,11 +13,23 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(()=> {
+    axios.get(`https://swapi.co/api/people/`).then(resp => {
+      setStarWarsChar(resp.data.results);
+      console.log(resp.data.results);
+
+     });
+  }, []);
+
+
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
-}
+      <div className="App">
+        <h1 className="Header">React Wars</h1>
+      {starWarschar.map(char => {
+      return <StarWarsChar char={char} key={char.name} />
+      })}
+   </div>
+    );
+  }
 
 export default App;
